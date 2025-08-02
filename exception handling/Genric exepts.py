feead0b8bec97ee73013ad7e066bada002ebaec1
Kuldeep Yadav <kuldeepyadav65088@gmail.com>
2025-08-02 16:27:39 +0530

    changes

--- exception handling/Genric exepts.py
+++ exception handling/Genric exepts.py
@@ -3,12 +3,6 @@ try:
     n2=int(input("Enter second number:"))
     r=n1/n2
     print('Result:',r)
-
-except ValueError:
-    print("Not a number")
-except ZeroDivisionError:
-    print("can't divide by zero")
 except Exception:
     print("something went wrong")
-print("Hello")
-print('bye')
+print("hello")
